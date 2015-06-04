065cc0eede56675f999d5eade0aa32e581a2d68a
Erick Peirson <erickpeirson@Ericks-MacBook-Air-2.local>
2015-06-03 20:13:57 -0700

    corpus

--- tests/refactor/test_classes_corpus.py
+++ tests/refactor/test_classes_corpus.py
@@ -2,8 +2,8 @@ import sys
 sys.path.append('../tethne')
 
 import unittest
-from tethne.readers.wos_refactor import WoSParser
-from tethne.classes.corpus import Corpus
+from tethne.readers.wos import read
+from tethne import Corpus, Paper
 from tethne.utilities import _iterable
 
 datapath = './tests/data/refactor_data/wos.txt'
@@ -11,8 +11,7 @@ datapath = './tests/data/refactor_data/wos.txt'
 
 class TestCorpus(unittest.TestCase):
 	def setUp(self):
-		parser = WoSParser(datapath)
-		self.papers = parser.parse()
+		self.papers = read(datapath, corpus=False)
 		
 	def test_init(self):
 		"""
@@ -26,6 +25,7 @@ class TestCorpus(unittest.TestCase):
 									'{0}: {1}'.format(E.__class__.__name__,
                                                       E.message)])
 			self.fail(failure_msg)
+
 
 	def test_indexing(self):
 		"""
@@ -44,6 +44,43 @@ class TestCorpus(unittest.TestCase):
 							    for o in _iterable(getattr(p, field))]))
 			self.assertEqual(len(corpus.indices[field]), expected,
 							 'Index for {0} is the wrong size.'.format(field))
+	 
+	
+	def test_slice(self):
+		corpus = Corpus(self.papers, index_by='wosid')
+		for papers in corpus.slice():
+			self.assertIsInstance(papers, list)
+			self.assertIsInstance(papers[0], Paper)
+
+
+	def test_slice_sliding(self):
+		corpus = Corpus(self.papers, index_by='wosid')
+		allpapers = [papers for papers in corpus.slice(window_size=2)]
+		
+		self.assertEqual(len(allpapers[0]), 10)
+
+
+	def test_slice_larger(self):
+		corpus = Corpus(self.papers, index_by='wosid')
+		allpapers = [papers for papers in corpus.slice(window_size=2, step_size=2)]
+		
+		self.assertEqual(len(allpapers[0]), 10)
+		
+
+	def test_distribution(self):
+		corpus = Corpus(self.papers, index_by='wosid')	
+		values = corpus.distribution()
+		
+		self.assertListEqual(values, [5, 5])
+		
+	
+	def test_feature_distribution(self):
+		corpus = Corpus(self.papers, index_by='wosid')	
+		values = corpus.feature_distribution('authors', ('ZENG', 'EDDY Y'))
+
+		self.assertListEqual(values, [0, 1])
+
+		
 
 
 
--- tests/refactor/test_readers_wos.py
+++ tests/refactor/test_readers_wos.py
@@ -2,56 +2,68 @@ import sys
 sys.path.append('../tethne')
 
 import unittest
-from tethne.readers.wos_refactor import WoSParser
+from tethne.readers.wos import WoSParser, read
+from tethne import Corpus, Paper
 
 datapath = './tests/data/refactor_data/wos.txt'
 
 
 class TestWoSParser(unittest.TestCase):
-    def test_parse(self):
-        parser = WoSParser(datapath)
-        parser.parse()
-        
-        # Check data types for the most common fields.
-        derror = "{0} should be {1}, but is {2}"
-        for entry in parser.data:
-            self.assertIsInstance(entry.date, int,
-                                  derror.format('date', 'int', type(entry.date)))
-            self.assertIsInstance(entry.authors_full, list,
-                                  derror.format('authors_full', 'list', 
-                                                type(entry.authors_full)))
-            self.assertIsInstance(entry.journal, str,
-                                  derror.format('journal', 'str', type(entry.journal)))
-            self.assertIsInstance(entry.abstract, str,
-                                  derror.format('abstract', 'str', type(entry.abstract)))
-            self.assertIsInstance(entry.authorKeywords, list,
-                                  derror.format('authorKeywords', 'list', 
-                                                type(entry.authorKeywords)))
-            self.assertIsInstance(entry.keywordsPlus, list,
-                                  derror.format('keywordsPlus', 'list', 
-                                                type(entry.keywordsPlus)))	
-            self.assertIsInstance(entry.doi, str,
-                                  derror.format('doi', 'str', type(entry.doi)))
-            self.assertIsInstance(entry.volume, str,
-                                  derror.format('volume', 'str', type(entry.volume)))		
-        
-        # Check integrity of tag-to-field mapping.
-        for tag, attr in parser.tags.iteritems():
-            self.assertFalse(hasattr(entry, tag), 
-                             ' '.join(['Tag-to-field translation is corrupted.',
-                                       '{0} should map to'.format(tag),
-                                       '{0}, but does not.'.format(attr)]))
+	def test_read(self):
+		corpus = read(datapath)
+		self.assertIsInstance(corpus, Corpus)
 
-        # Check number of records.
-        self.assertEqual(len(parser.data), 10, 
-                         'Expected 10 entries, but found {0}.'.format(len(parser.data)))
 
-        self.assertTrue(hasattr(parser.data[0], 'citedReferences'))
-        for cr in parser.data[0].citedReferences:
-            self.assertTrue(hasattr(cr, 'date'))
-            if cr.date:
-                self.assertIsInstance(cr.date, int)
-            self.assertTrue(hasattr(cr, 'journal'))
+	def test_read_nocorpus(self):
+		papers = read(datapath, corpus=False)
+		self.assertIsInstance(papers, list)
+		self.assertIsInstance(papers[0], Paper)
+
+
+	def test_parse(self):
+		parser = WoSParser(datapath)
+		parser.parse()
+	
+		# Check data types for the most common fields.
+		derror = "{0} should be {1}, but is {2}"
+		for entry in parser.data:
+			self.assertIsInstance(entry.date, int,
+								  derror.format('date', 'int', type(entry.date)))
+			self.assertIsInstance(entry.authors_full, list,
+								  derror.format('authors_full', 'list', 
+												type(entry.authors_full)))
+			self.assertIsInstance(entry.journal, str,
+								  derror.format('journal', 'str', type(entry.journal)))
+			self.assertIsInstance(entry.abstract, str,
+								  derror.format('abstract', 'str', type(entry.abstract)))
+			self.assertIsInstance(entry.authorKeywords, list,
+								  derror.format('authorKeywords', 'list', 
+												type(entry.authorKeywords)))
+			self.assertIsInstance(entry.keywordsPlus, list,
+								  derror.format('keywordsPlus', 'list', 
+												type(entry.keywordsPlus)))	
+			self.assertIsInstance(entry.doi, str,
+								  derror.format('doi', 'str', type(entry.doi)))
+			self.assertIsInstance(entry.volume, str,
+								  derror.format('volume', 'str', type(entry.volume)))		
+	
+		# Check integrity of tag-to-field mapping.
+		for tag, attr in parser.tags.iteritems():
+			self.assertFalse(hasattr(entry, tag), 
+							 ' '.join(['Tag-to-field translation is corrupted.',
+									   '{0} should map to'.format(tag),
+									   '{0}, but does not.'.format(attr)]))
+
+		# Check number of records.
+		self.assertEqual(len(parser.data), 10, 
+						 'Expected 10 entries, but found {0}.'.format(len(parser.data)))
+
+		self.assertTrue(hasattr(parser.data[0], 'citedReferences'))
+		for cr in parser.data[0].citedReferences:
+			self.assertTrue(hasattr(cr, 'date'))
+			if cr.date:
+				self.assertIsInstance(cr.date, int)
+			self.assertTrue(hasattr(cr, 'journal'))
 
 
 if __name__ == '__main__':
